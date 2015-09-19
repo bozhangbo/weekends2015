081284897534bdf9dbbaa361bcd8577d50ecfa88
Yang Guang <gyang@phy.ecnu.edu.cn>
2015-09-19 16:33:26 +0800

    Add project Draw
    Add project Draw to demostrate GDI+ capabilities.

---
+++ Draw/DrawView.cpp
@@ -0,0 +1,148 @@
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface 
+// (the "Fluent UI") and is provided only as referential material to supplement the 
+// Microsoft Foundation Classes Reference and related electronic documentation 
+// included with the MFC C++ library software.  
+// License terms to copy, use or distribute the Fluent UI are available separately.  
+// To learn more about our Fluent UI licensing program, please visit 
+// http://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// DrawView.cpp : implementation of the CDrawView class
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "Draw.h"
+#endif
+
+#include "DrawDoc.h"
+#include "DrawView.h"
+
+#include <gdiplus.h>
+using namespace Gdiplus;
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CDrawView
+
+IMPLEMENT_DYNCREATE(CDrawView, CView)
+
+BEGIN_MESSAGE_MAP(CDrawView, CView)
+	// Standard printing commands
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CDrawView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CDrawView construction/destruction
+
+CDrawView::CDrawView()
+{
+	// TODO: add construction code here
+
+}
+
+CDrawView::~CDrawView()
+{
+}
+
+BOOL CDrawView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: Modify the Window class or styles here by modifying
+	//  the CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CDrawView drawing
+
+void CDrawView::OnDraw(CDC* pDC)
+{
+	CDrawDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	CRect client_rect;
+	GetClientRect(client_rect);
+
+	Graphics my_graphics(pDC->m_hDC);
+	Pen pen(Color::Red);
+
+	my_graphics.DrawLine(&pen, 0, 0, client_rect.right, client_rect.bottom);
+	my_graphics.DrawLine(&pen, 0, client_rect.bottom, client_rect.right, client_rect.top);
+}
+
+
+
+// CDrawView printing
+
+
+void CDrawView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CDrawView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// default preparation
+	return DoPreparePrinting(pInfo);
+}
+
+void CDrawView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add extra initialization before printing
+}
+
+void CDrawView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add cleanup after printing
+}
+
+void CDrawView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CDrawView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CDrawView diagnostics
+
+#ifdef _DEBUG
+void CDrawView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CDrawView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CDrawDoc* CDrawView::GetDocument() const // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CDrawDoc)));
+	return (CDrawDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CDrawView message handlers
